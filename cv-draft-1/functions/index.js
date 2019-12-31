adbd64fec2f3af3a5ecf676cb5333f5583a2be12
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2019-12-31 11:57:47 +0530

    Update index.js

--- cv-draft-1/functions/index.js
+++ cv-draft-1/functions/index.js
@@ -13,24 +13,25 @@ Carousel,
 // Import the firebase-functions package for deployment.
 const functions = require('firebase-functions');
 
-// Define a mapping of fake color strings to basic card objects.
+// Define a mapping of profile Map strings to basic card objects.
 const profileMap = {
   'profile': {
-    title: 'Image',
-    text: 'Hi!! i am Shreyans',
+    title: 'Shreyans Jain',
+    text: 'Hi!! I am Shreyans',
     image: {
-      url: 'https://avatars2.githubusercontent.com/u/50544190?s=460&v=4',
+      url: 'https://raw.githubusercontent.com/Shreyans13/Actions-on-google/master/cv-draft-1/resources/profileImage.jpeg',
       accessibilityText: 'Profile Image',
     },
     display: 'WHITE',
   },
 };
+// Define a mapping of contact Map strings to basic card objects.
 const contactMap = {
   'contactProfile': {
     title: 'Contact Information',
     text: 'Shreyans Jain  \nEmail:- Shreyans1313@gmail.com',
     image: {
-      url: 'https://avatars2.githubusercontent.com/u/58992004?s=400&u=54a495a3893dccf3298ece65abdb2cb0805d8379&v=4',
+      url: 'https://raw.githubusercontent.com/Shreyans13/Actions-on-google/master/cv-draft-1/resources/profileImage.jpeg',
       accessibilityText: 'contact image',
     },
     display: 'WHITE',
@@ -60,42 +61,48 @@ app.intent('About', (conv, {about}) => {
 conv.ask(new Suggestions('About', 'Skills', 'Qualifications','Projects','Contact','Exit')); 
 });
 
-
+// Handle the Dialogflow intent named 'Skills'.
+// The intent collects a parameter named 'skills'.
 app.intent('Skills',(conv, {skills})=>{
 	conv.ask("I am a Web Developer \nOn the technical front, I have good command over the various programming languages like \nJava, \nHTML 5, \nCSS and \nJavaScript");
 	
 	conv.ask(new Suggestions('About', 'Skills', 'Qualifications','Projects','Contact','Exit'));
 });
 
+// Handle the Dialogflow intent named 'Quaifications'.
+// The intent collects a parameter named 'qualifications'.
 app.intent('Qualifications',(conv, {qualifications})=>{
 	conv.ask("BE-CSE Chandigarh University \nSenior Secondary School Andhra English School \nSecondary School DBMS English School, ");
 
 	conv.ask(new Suggestions('About', 'Skills', 'Qualifications','Projects','Contact','Exit'));
 
 });
-	  const projectInformationCarousel = () => {
-	  const carousel = new Carousel({
-	   items: {
-	     'github repository': {
-	       title: 'Github repository',
-	       synonyms: ['github', 'repository'],
-	       image: new Image({
-	         url: 'https://raw.githubusercontent.com/Shreyans13/Actions-on-google/master/cv-draft-1/resources/images.jpeg',
-	         alt: 'Open Sources Contibutions',
-	       }),
-	     },
-	     'actions on google': {
-       title: 'Actins on google[Resume]',
+// Define a Carousel of projectInformation strings to basic card objects.
+  const projectInformationCarousel = () => {
+  const carousel = new Carousel({
+   items: {
+     'github repository': {
+       title: 'Github repository',
+       synonyms: ['github', 'repository'],
+       image: new Image({
+         url: 'https://raw.githubusercontent.com/Shreyans13/Actions-on-google/master/cv-draft-1/resources/images.jpeg',
+         alt: 'Open Sources Contibutions',
+       }),
+     },
+     'actions on google': {
+   	   title: 'Actions on google[Resume]',
        synonyms: ['actions', 'google', 'resumae'],
        image: new Image({
          url: 'https://raw.githubusercontent.com/Shreyans13/Actions-on-google/master/cv-draft-1/resources/actionsOnGoogle.jpeg',
          alt: 'Actions on google[Resume]',
        }),
      },
-	 }});
-	 return carousel;
-	};
+ }});
+ return carousel;
+};
 
+// Handle the Dialogflow intent named 'Projects'.
+// The intent collects a parameter named 'project'.
 app.intent('Projects',(conv, {project})=>{
 	conv.ask("I love programming and also contribute to open source. Here are my projects");
 	
@@ -106,10 +113,11 @@ app.intent('Projects',(conv, {project})=>{
 
 });
 
+// Handle the Dialogflow intent named 'Contact'.
+// The intent collects a parameter named 'contact'.
 app.intent('Contact',(conv, {contact})=>{
 	const y="contactProfile";
-conv.ask('Here`s my contact info',new BasicCard(contactMap[y]));
-  // conv.ask(new BasicCard(contactMap[z]));
+conv.ask("Here's my contact info",new BasicCard(contactMap[y]));
 	
 	conv.ask(new Suggestions('About', 'Skills', 'Qualifications','Projects','Contact','Exit'));
 });
